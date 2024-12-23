8933af6319a4b5b1994de8f85172dc0e28bfb2b1
Chen-kaduri <chenkaduri@gmail.com>
2024-12-23 19:41:34 +0200

    typo

--- Message.java
+++ Message.java
@@ -11,7 +11,7 @@ public class Message {
 
     public Message(byte[] data) {
         this.data = data;
-        //המרה לביית בקידוד UTF8
+        //המרה לבייט בקידוד UTF8
         this.asText = new String(data,java.nio.charset.StandardCharsets.UTF_8);
         this.asDouble = tryparseFromText(this.asText);
         this.date = new Date();
