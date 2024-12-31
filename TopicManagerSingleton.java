db4a42780e70b870b0088b6781f79e2640300dfd
Chen-kaduri <chenkaduri@gmail.com>
2024-12-31 19:33:04 +0200

    create TopicManagerSingleton
    Implementation of the TopicManagerSingleton method with all the required functions

--- TopicManagerSingleton.java
+++ TopicManagerSingleton.java
@@ -1,10 +1,39 @@
 package test;
 
+import java.util.Collection;
+import java.util.Map;
 
-public class TopicManagerSingleton {
+public class TopicManagerSingleton
+{
+    public static class TopicManager {
+        Map<String, Topic> topics;
+        private static final TopicManager instance = new TopicManager();
 
-    public static class TopicManager{
+        private TopicManager(){
+            topics = new HashMap<>();
+        }
+
+        public Topic getTopic(String topicName){
+            //Checking if it exists and if it will not be produced
+            if(!topics.containsKey(topicName))
+            {
+                topics.put(topicName, new Topic(topicName));
+            }
+            return topics.get(topicName);
+        }
+
+        public Collection<Topic> getTopics(){
+            return topics.values();
+        }
+
+        public void clear(){
+            topics.clear();
+        }
+
     }
 
+    public static TopicManager getInstance() {
+        return TopicManager.instance;
+     }
     
 }
